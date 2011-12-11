6f343459d8d8d1e71fbde14465f872b886e15f93
Samy Dindane <samy@dindane.com>
2011-12-11 17:59:50 +0100

    Optimized the code and added the support of the case where the message isn't a string or array

--- FlashMessages.php
+++ FlashMessages.php
@@ -1,22 +1,35 @@
 <?php
 
-class Dinduks_Controller_Plugin_FlashMessage extends Zend_Controller_Plugin_Abstract
+/**
+ * @author  Samy Dindane <samy@dindane.com>
+ * @link    https://github.com/Dinduks/ZF-FlashMessagesPlugin
+ * @package ZF-FlashMessagesPlugin
+ */
+class Dinduks_Controller_Plugin_FlashMessages 
+      extends Zend_Controller_Plugin_Abstract
 {
 
+    /**
+     * @param Zend_Controller_Request_Abstract $request
+     * @return array 
+     */
     public function preDispatch(Zend_Controller_Request_Abstract $request)
     {
         $messages = Zend_Controller_Action_HelperBroker::
                 getStaticHelper('FlashMessenger')->getMessages();
 
-        if (!empty($messages)) {
-            foreach ($messages as &$message) {
-                if (is_array($message)) {
-                    if (array_values($message) === $message) {
-                        $message = array('' => $message[key($message)]);
-                    }
-                } else {
-                    $message = array('' => $message);
+        foreach ($messages as &$message) {
+            if (is_array($message)) {
+                if (array_values($message) === $message) {
+                    $message = array('' => $message[key($message)]);
                 }
+            } else if (is_string($message)) {
+                $message = array('' => $message);
+            } else {
+                throw new Exception(
+                    "Flash messages can be arrays and strings only! \n
+                     '$message' is not a valid value."
+                );
             }
 
             $viewRenderer = Zend_Controller_Action_HelperBroker::
@@ -24,6 +37,8 @@ class Dinduks_Controller_Plugin_FlashMessage extends Zend_Controller_Plugin_Abst
             $viewRenderer->init();
             $viewRenderer->view->flashMessages = $messages;
         }
+        
+        return $messages;
     }
 
 }
